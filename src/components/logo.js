bf7ce33b167302345ee04b1b09589400e7764ffb
jhon sapuyes <jhonsapuyes1986@gmail.com>
2022-12-16 23:41:00 -0500

    first commit

---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+
+import background from "../img/logo.png";
+
+
+const Logo = 
+    <>
+    <div class="App-logo" style={{ backgroundImage: `url(${background})` }}>
+    </div>
+    </>
+
+export default Logo;
